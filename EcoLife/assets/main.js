dc88bf64cd193b81a84d05c53b84ae7d7b0bac86
Reema7Abdullah <reema7iabdullah@gmail.com>
2024-12-24 21:21:42 +0300

    Add files via upload

---
+++ EcoLife/assets/main.js
@@ -0,0 +1,16 @@
+// when click toggle the content
+document.getElementById("banner").onclick = function() {
+    window.location.hash = "#banner";
+};
+
+document.getElementById("about").onclick = function() {
+    window.location.hash = "index.html#about";
+};
+
+document.getElementById("products").onclick = function() {
+    window.location.hash = "#products";
+};
+
+document.getElementById("contact-us").onclick = function() {
+    window.location.hash = "#contact-us";
+};
